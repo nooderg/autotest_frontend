5dd1698584504cf8433791c4e4af63d6f3d4b686
Artpou <arthur.poullin99@gmail.com>
2022-03-09 16:30:30 +0100

    🎨 Add profilMenu in Appbaar

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { Login } from './Login/Login';
 export { Appbar } from './Appbar/Appbar';
 export {LoginButton} from './LoginButton/LoginButton'
+export {ProfilIconMenu} from './ProfilIconMenu/ProfilIconMenu'
