339019316105c1942f30dbd44d7b28d42b1bc768
Artpou <arthur.poullin99@gmail.com>
2022-03-09 16:29:39 +0100

    ✨ ProfilPage

---
+++ src/pages/ProfilPage/ProfilPage.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import styles from "./LoginPage.module.css";
+import {
+  Card,
+  CardContent,
+  Container
+} from "@mui/material";
+import { Login } from "../../components";
+
+
+export function ProfilPage() {
+
+  return (
+    <Container
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      Profil Page
+    </Container>
+  );
+}
