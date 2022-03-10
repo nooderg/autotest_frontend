ad8178ea7cd6577d226235f11f899ce10cb29f86
Guillaume Marcel <marcelguillaume54@gmail.com>
2022-03-10 13:35:06 +0100

    add base64 encode to file

--- src/helper/formValidation.ts
+++ src/helper/formValidation.ts
@@ -4,3 +4,13 @@ export const isEmail = (email: string): boolean => {
     return regex.test(email);
   };
   
+  export const fileToBase64 = (file: File | Blob): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+     resolve(reader.result as string);
+    };
+
+    reader.readAsDataURL(file);
+    reader.onerror = reject;
+  });
